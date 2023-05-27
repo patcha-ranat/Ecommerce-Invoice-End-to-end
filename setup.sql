63520562297ddd77835073f9716562506e66a6b2
Patcharanat <patcha-ranat@outlook.com>
2023-05-27 23:16:59 +0700

    set postgres source

---
+++ setup.sql
@@ -0,0 +1,18 @@
+-- CREATE DATABASE mydatabase;
+CREATE SCHEMA IF NOT EXISTS myschema;
+
+DROP TABLE IF EXISTS myschema.ecomm_invoice;
+
+CREATE TABLE myschema.ecomm_invoice (
+    InvoiceNo TEXT,
+    StockCode TEXT,
+    "Description" TEXT,
+    Quantity INT,
+    InvoiceDate TIMESTAMP,
+    UnitPrice FLOAT,
+    CustomerID TEXT,
+    Country TEXT
+);
+
+COPY myschema.ecomm_invoice
+FROM '/data/cleaned_data.csv' DELIMITER ',' CSV HEADER;
