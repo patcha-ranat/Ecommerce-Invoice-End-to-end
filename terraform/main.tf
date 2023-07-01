24df3d28574b9348aa87e80b5f2ef89c118a375f
Patcharanat <patcha-ranat@outlook.com>
2023-07-01 12:23:52 +0700

    add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,106 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("./credentials/gcs_credentials.json")
+  project     = var.project
+  region      = var.region
+}
+
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "gcs_bucket" {
+  name          = local.data_lake_bucket
+  location      = var.region
+  
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+  force_destroy = true
+  public_access_prevention = "enforced"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 7  // days
+    }
+  }
+}
+
+# skip this if you manually create service account via GCP UI (and granted permission)
+# resource "google_storage_bucket_iam_binding" "bucket_iam_binding" {
+#   bucket = google_storage_bucket.gcs_bucket.name
+#   role   = "roles/storage.admin"
+
+#   members = [
+#     "user:<USER_EMAIL>",
+#     "group:<GROUP_EMAIL>",
+#     "serviceAccount:<SERVICE_ACCOUNT_EMAIL>",
+#   ]
+# }
+
+# resource "google_storage_bucket_iam_member" "bucket_iam_member" {
+#   bucket = google_storage_bucket.bucket.name
+#   role   = "roles/storage.objectViewer"
+#   member = "allUsers"
+# }
+
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.BQ_DATASET
+  project    = var.project
+  location   = var.region
+}
+
+resource "google_bigquery_table" "table" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  project    = var.project
+  table_id   = var.TABLE_ID
+
+  schema {
+    fields {
+      name = "InvoiceNO"
+      type = "STRING"
+    }
+    fields {
+      name = "StockCode"
+      type = "STRING"
+    }
+    fields {
+      name = "Description"
+      type = "STRING"
+    }
+    fields {
+      name = "Quantity"
+      type = "INTEGER"
+    }
+    fields {
+      name = "InvoiceDate"
+      type = "TIMESTAMP"
+    }
+    fields {
+      name = "UnitPrice"
+      type = "FLOAT"
+    }
+    fields {
+      name = "CustomerID"
+      type = "INTEGER"
+    }
+    fields {
+      name = "Country"
+      type = "STRING"
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,36 @@
+locals {
+  data_lake_bucket = "ecomm-invoice-data-lake-bucket"
+}
+
+variable "project" {
+  description = "Your Project ID"
+  default = "fabled-rookery-386802"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "asia-southeast1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that cleaned data (from GCS) will be written to"
+  type = string
+  default = "ecomm_invoice"
+}
+
+variable "TABLE_ID" {
+  description = "your table name in BigQuery"
+  type = string
+  default = "ecomm_invoice_transaction"
+}
+
+# variable "BUCKET_NAME" {
+#     description = "Unique name for your bucket"
+#     defualt = "ecomm-invoice-data-lake-bucket"
+# } -> use local instead
