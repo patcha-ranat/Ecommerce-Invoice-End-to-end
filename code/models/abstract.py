9b5fe290d0150599c5e1adb7a35837bf0987a1dd
ken-patcharanat <patcha-ranat@outlook.com>
2024-10-14 20:29:59 +0700

    improve io factory

--- code/models/abstract.py
+++ code/models/abstract.py
@@ -2,27 +2,38 @@ from abc import ABC, abstractmethod
 
 
 # Abstract Class
-class BaseInputReader(ABC):
-    """Abstract class for reading input data depending on source"""
-    def is_data_exists(self):
+class AbstractIOReaderWriter(ABC):
+    """Abstract class for reading/writing data depending on source type and method"""
+
+    def is_db_exists(self):
         pass
+
     def connect_db(self):
         pass
-    def init_data(self):
+
+    def render_sql(self):
         pass
+
+    def init_db(self):
+        pass
+
     def read(self):
         pass
 
-
-class BaseOutputWriter(ABC):
-    """Abstract class for writing output data depending on destination"""
-    @abstractmethod
     def write(self):
         pass
 
 
-class BaseMlService(ABC):
+class AbstractIOProcessor:
+    """Abstract class as an entrypoint for choosing a type of Reader/Writer then parse parameters to them"""
+
+    def process(self):
+        pass
+
+
+class AbstractMlService(ABC):
     """Abstract class for managing ml-related processes"""
+
     @abstractmethod
     def train(self):
         pass
--- code/models/io_services.py
+++ code/models/io_services.py
@@ -5,115 +5,115 @@ import logging
 import pandas as pd
 import duckdb
 
-from abstract import BaseInputReader, BaseOutputWriter
+from abstract import AbstractIOReaderWriter
 
 
-class LocalInputReader(BaseInputReader):
+class BaseIOReaderWriter(AbstractIOReaderWriter):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def is_db_exists(input_path: str) -> bool:
+        # os.path.isfile(path=input_path) -> bool
+        data_file = Path(input_path)
+        return data_file.is_file()
+
+    @staticmethod
+    def connect_db(input_path: str) -> tuple:
+        con = duckdb.connect(input_path)
+        cursor = con.cursor()
+        return (cursor, con)
+
+    @staticmethod
+    def render_sql(file_path: str) -> str:
+        with open(file_path, "r") as sql_f:
+            statement = sql_f.read()
+        sql_f.close()
+        return statement
+
+
+class LocalInputReader(BaseIOReaderWriter):
     """
-    Input Reader to read input from various source depending on choice.
-
-    :param method: *(Required)* Parameter to specify type of source to read from.\n
-        Example:\n
-            method = "db"\n
-        Parameter Choices: ["db", "filesystem"]
+    :param method: *(Required)* Source type to read from.\n
+        Parameters choices: ['db', 'filesystem']
     :type method: str
 
-    :param path: *(Required)* Parameter to specify relative path to input data, regardless data model.\n
-        Example:\n
-            path = "path/to/local/database.db"\n
-            path = "path/to/local/file/input.parquet"\n
-    :type path: str
+    :param input_path: *(Required)* Path to a source file or a local db file
+    :type input_path: str
 
-    :param data_model: *(Optional)* Parameter to specify data or table to read from database in `schema.table` format. (unused for filesystem)\n
-        Example: data_model = "ecomm_gold.sales_transaction"\n
-        Parameter Choices: ["ecomm_gold.sales_transaction", "ecomm_gold.customer_rfm"]
-    :type data_model: str
-
-    :param init_script_path: *(Optional)* Parameter to customize init script path for 'db' method. default: 'init_script/duckdb.sql'\n
+    :param init_script_path: *(Optional)* Path to initial script for database initialization
     :type init_script_path: str
 
-    :return: Input data as a DataFrame
-    :rtype: pd.DataFrame
-
+    :param init_data_path: *(Optional)* Path to initial data path for database initialization
+    :type init_data_path: str
     """
 
     def __init__(
         self,
         method: str,
-        path: str,
-        data_model: str = None,
-        init_script_path: str = "init_script/duckdb.sql",
-    ) -> pd.DataFrame:
+        input_path: str,
+        init_script_path: str = "db/sql/init_sales.sql",
+        init_data_path: str = "../../data/ecomm_invoice_transaction.parquet",
+    ):
         super().__init__()
         self.method = method
-        self.path = path
-        self.data_model = data_model
+        self.input_path = input_path
+        # sql_path
+        # data_model
         self.init_script_path = init_script_path
+        self.init_data_path = init_data_path
 
-        if self.is_data_exists():
+        if self.is_db_exists(input_path):
             pass
-        else:
-            self.init_data()
-
-        return self.read()
-
-    @staticmethod
-    def is_data_exists(path: str) -> bool:
-        # os.path.isfile(path=path) -> bool
-        data_file = Path(path)
-        return data_file.is_file()
-
-    @staticmethod
-    def connect_db(path: str):
-        con = duckdb.connect(path)
-        cursor = con.cursor()
-        return cursor
-
-    def init_data(self) -> None:
-        if self.method == "db":
-            # connect local db
-            cursor = self.connect_db(path=self.path)
-
-            # read initial script
-            with open(self.init_script_path, "r") as sql_f:
-                statement = sql_f.read()
-
-            # execute sql
-            cursor.execute(statement)
-            # .execute() return connection
-            # .sql() return relation (table)
-
-            # close file
-            sql_f.close()
-
-        elif self.method == "filesystem":
-            logging.info(
+        elif method == "db":
+            self.init_db()
+        elif method == "filesystem":
+            logging.warning(
                 "`method` parameter is given as 'filesystem', no initialization required"
             )
         else:
-            raise Exception("Unacceptable `method` argument for Reader.")
+            raise Exception(
+                "Database is not exist. Unacceptable `method` argument for Reader."
+            )
+        return self
 
-    def read(self) -> pd.DataFrame:
+    def init_db(self) -> None:
+        # connect local db
+        cursor, con = self.connect_db(path=self.input_path)
+
+        # read initial script
+        statement = self.render_sql(self.init_script_path)
+        # execute sql
+        sql_params = {
+            "data_model": "ecomm_gold.sales_transaction",  # only sales data is acceptable for initialization
+            "data_path": self.init_data_path,
+        }
+        cursor.execute(statement, parameters=sql_params)
+        # .execute() return connection
+        # .sql() return relation (table)
+        con.close()
+
+    def read(self, sql_path: str = None, sql_params: dict = None) -> pd.DataFrame:
         if self.method == "db":
             # connect db
-            cursor = self.connect_db(path=self.path)
+            cursor, con = self.connect_db(path=sql_path)
 
             # execute reading input statement, then load to pandas dataframe
-            df = cursor.sql(f"SELECT * FROM {self.data_model}").to_df()
+            statement = self.render_sql(sql_path)
+            df = cursor.sql(statement, parameters=sql_params).to_df()
 
+            con.close()
             return df
 
         elif self.method == "filesystem":
             # check redundant argument
-            if self.data_model:
-                logging.warning(
-                    "`data_model` argument is not used, reading filesystem path."
-                )
+            logging.warning("Reading filesystem path with `input_path` parameter.")
 
             # reading file
-            logging.info(f"Reading file: {Path(self.path).name}")
+            logging.info(f"Reading file: {Path(self.input_path).name}")
             # Path().stem, Path().suffix, Path().name, Path().parent
-            df = pd.read_parquet(self.path)
+            df = pd.read_parquet(self.input_path)
+            logging.info(f"Successfully read file: {Path(self.input_path).name}")
 
             return df
 
@@ -121,32 +121,28 @@ class LocalInputReader(BaseInputReader):
             raise Exception("Unacceptable `method` argument for Reader.")
 
 
-class GCPInputReader(BaseInputReader):
+class GCPInputReader(BaseIOReaderWriter):
     pass
 
 
-class DockerDatabaseInputReader(BaseInputReader):
+class DockerDatabaseInputReader(BaseIOReaderWriter):
     pass
 
 
 class InputProcessor:
     """
-    Entrypoint for InputReader instance, selecting connection/environment type by given parameter.
+    Entrypoint for InputReader instance, selecting connection/environment type by given parameters.
     """
 
     def __init__(
         self,
         source_type: str,
         method: str,
-        path: str = None,
-        data_model: str = None,
-        init_script_path: str = None,
+        input_path: str = None,
     ):
         self.source_type = source_type
         self.method = method
-        self.path = path
-        self.data_model = data_model
-        self.init_script_path = init_script_path
+        self.input_path = input_path
         self.factory = {
             "local": LocalInputReader,
             "postgresql": DockerDatabaseInputReader,
@@ -157,60 +153,95 @@ class InputProcessor:
         reader_instance = self.factory.get(self.source_type)
         reader_args = {
             "method": self.method,
-            "path": self.path,
-            "data_model": self.data_model,
-            "init_script_path": self.init_script_path,
+            "input_path": self.input_path,
         }
-        input_df = reader_instance(**reader_args)
+        reader = reader_instance(**reader_args)
 
         if reader_instance:
-            return input_df
+            return reader
         else:
             raise Exception("No InputReader assigned in InputProcessor factory")
 
 
-class LocalOutputWriter(BaseOutputWriter):
+class LocalOutputWriter(BaseIOReaderWriter):
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        method: str,
+        output_path: str,
+    ) -> None:
+        super().__init__()
+        self.df = df
+        self.method = method
+        self.output_path = output_path
+
+        return self.write()
+
+    def write(self, sql_path: str = None, sql_params: dict = None) -> None:
+        if self.method == "db":
+            # connect db
+            cursor, con = self.connect_db(path=self.output_path)
+
+            # reading sql file rendering sql statement
+            statement = self.render_sql(file_path=sql_path)
+
+            # execute writing output statement to load pandas DataFrame to duckdb database
+            # https://duckdb.org/docs/guides/python/import_pandas.html
+            in_memory_df = self.df
+            in_memory_df_param = "in_memory_df"
+            sql_params = {**sql_params, in_memory_df_param: in_memory_df}
+            cursor.sql(statement, parameters=sql_params)
+
+            con.close()
+            logging.info(
+                f"Successfully write output file: {Path(self.output_path).name}"
+            )
+            return 1
+
+        elif self.method == "filesystem":
+
+            # writing file
+            logging.info(f"Writing file: {Path(self.output_path).name}")
+            self.df.to_parquet(self.output_path)
+
+            return 1
+
+        else:
+            raise Exception("Unacceptable `method` argument for Reader.")
+
+
+class GCPOutputWriter(BaseIOReaderWriter):
     pass
 
 
-class GCPOutputWriter(BaseOutputWriter):
+class DockerDatabaseOutputWriter(BaseIOReaderWriter):
     pass
 
 
-class DockerDatabaseOutputWriter(BaseOutputWriter):
-    pass
+class OutputProcessor:
+    def __init__(
+        self, target_type: str, method: str, output_path: str, df: pd.DataFrame
+    ):
+        self.target_type = target_type
+        self.method = method
+        self.output_path = output_path
+        self.df = df
+        self.factory = {
+            "local": LocalOutputWriter,
+            "postgresql": DockerDatabaseOutputWriter,
+            "gcp": GCPOutputWriter,
+        }
 
+    def process(self):
+        writer_instance = self.factory.get(self.target_type)
+        writer_args = {
+            "df": self.df,
+            "method": self.method,
+            "output_path": self.output_path,
+        }
+        writer = writer_instance(**writer_args)
 
-# class OutputProcessor:
-#     def __init__(
-#         self,
-#         target_type: str,
-#         method: str,
-#         path: str = None,
-#         data_model: str = None,
-#         init_script_path: str = None,
-#     ):
-#         self.source_type = target_type
-#         self.method = method
-#         self.path = path
-#         self.data_model = data_model
-#         self.init_script_path = init_script_path
-#         self.factory = {
-#             "local": LocalOutputWriter,
-#             "postgresql": DockerDatabaseOutputWriter,
-#             "gcp": GCPOutputWriter,
-#         }
-
-#     def process(self):
-#         writer_instance = self.factory.get(self.source_type)
-#         writer_args = {
-#             "method": self.method,
-#             "path": self.path,
-#             "data_model": self.data_model,
-#         }
-#         input_df = writer_instance(**writer_args)
-
-#         if writer_instance:
-#             return input_df
-#         else:
-#             raise Exception("No OutputWriter assigned in OutputProcessor factory")
+        if writer_instance:
+            return writer
+        else:
+            raise Exception("No OutputWriter assigned in OutputProcessor factory")
